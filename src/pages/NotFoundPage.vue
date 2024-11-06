f740db79f2085a4eb9bf3c461dd6a456f5c99a5f
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-11-06 15:12:52 -0600

    feat: Add AccessPage and NotFoundPage components; update router to include new routes

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid place-content-center h-screen text-3xl">
+    <h1><span class="font-bold">404</span> - Página no encontrada</h1>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ const routes = [
   { path: '/', component:  async () => await import('./pages/IndexPage.vue') },
   { path: '/about', component:  async () => await import('./pages/AboutPage.vue') },
   { path: '/stock/:name', component:  async () => await import('./pages/StockDetailPage.vue') },
+  { path: '/access', component:  async () => await import('./pages/AccessPage.vue') },
+  {path: '/:pathMatch(.*)*', component: async () => await import('./pages/NotFoundPage.vue')},
 ]
 
 export const router = createRouter({
