0d2b0a45434da5401a14284e2bf3c8360708a164
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-12-20 23:08:18 -0600

    fix comments in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,11 +18,8 @@ export default {
     plugin(({ addUtilities }) => {
       addUtilities({
         ".scrollbar-hide": {
-          /* IE and Edge */
           "-ms-overflow-style": "none",
-          /* Firefox */
           "scrollbar-width": "none",
-          /* Safari and Chrome */
           "&::-webkit-scrollbar": {
             display: "none",
           },
